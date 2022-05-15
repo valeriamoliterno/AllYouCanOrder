37ba24ce5d42601dfa758a9fad9b88c7cb22a631
valeriamoliterno <valeria.moliterno@studenti.unitn.it>
2022-05-15 17:01:49 +0200

    prima versione del file index.js  --Creato con Alice Rizzo

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require('./app/app.js');
+const mongoose = require('mongoose');
+
+/**
+ * https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
+ */
+const port = process.env.PORT || 3000;
+
+
+/**
+ * Configure mongoose
+ */
+// mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://AllYouCanOrder:AliValeGiuMa@cluster0.dxwja.mongodb.net/AllYouCanOrder?retryWrites=true&w=majority')
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+    
+});

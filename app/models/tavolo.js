c38701a6caa65df9fa6cdc0753dd48845cb5d61c
valeriamoliterno <valeria.moliterno@studenti.unitn.it>
2022-05-16 17:32:50 +0200

    seconda versione di tavolo.js

--- app/models/tavolo.js
+++ app/models/tavolo.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 const piatto= require('./models/piatto.js'); 
 
 module.exports = mongoose.model('Tavolo', new Schema({ 
+    idTavolo: Number,
     ordine: [piatto], // dopo aver inviato ordine;  
     chiamato: Boolean,  
     carrello: [piatto], //prima di inviare ordine 

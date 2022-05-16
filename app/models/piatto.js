6d92453b0a55f2b805caf115298b30b37d3c3fd4
valeriamoliterno <valeria.moliterno@studenti.unitn.it>
2022-05-16 17:32:27 +0200

    seconda versione di piatto.js

--- app/models/piatto.js
+++ app/models/piatto.js
@@ -3,8 +3,10 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model
 module.exports = mongoose.model('Piatto', new Schema({ 
-	stato: String,   
+    idPiatto: Number,
     nome: String,  
-    foto: String, //percorso  
+    prezzo: Number,
     descrizione: String, 
+    foto: String, //percorso  
+    stato: String,   
 }));

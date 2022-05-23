ff1d05494c885bc2b64181011db7ef3f37667cc4
giuliapiz <105370531+giuliapiz@users.noreply.github.com>
2022-05-23 19:23:47 +0200

    prima versione del file tavolos per la gestione dei tavoli

---
+++ app/tavolos.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const Tavolo = require('./models/tavolo');
+
+
+
+router.post('', async (req, res) => {
+
+	let tavolo = new Tavolo({
+        nome: req.body.nome,
+        chiamato:false
+    });
+    
+	tavolo = await tavolo.save();
+    
+    let tavoloId = tavolo.nome;
+
+    console.log('Tavolo salvato');
+
+
+    res.location("/api/v1/tavolos" + tavoloId).status(201).send();
+});
+
+
+module.exports = router;

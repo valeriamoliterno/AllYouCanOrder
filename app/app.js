bdbc4edd3c860807bd3caab238230098665173e0
giuliapiz <105370531+giuliapiz@users.noreply.github.com>
2022-05-26 11:03:37 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,15 +1,16 @@
+//Chiama il framework EXPRESS
 const express = require('express');
 const app = express();
 
 
 
-
+//Consentono di leggere il body delle response
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 
-/* Default 404 handler */
+//Gestisce i casi di errore
 app.use((req, res) => {
     res.status(404);
     res.json({ error: 'Not found' });
